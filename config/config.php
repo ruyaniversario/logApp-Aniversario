85d6bec1deda1c854d6dc773b90d79095f7ce58e
ruyaniversario <201440469@psu.palawan.edu.ph>
2021-06-01 15:49:20 +0800

    update config.php

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
-	
+	define('ROOT_URL', 'localhost/new/logApp-Aniversario/');
 	define('DB_HOST', 'sql6.freesqldatabase.com');
-	define('DB_USER', 'sql6415437');
-	define('DB_PASS', '3EhQ9Q2xWK');
-	define('DB_NAME', 'sql6415437');
+	define('DB_USER', 'sql6416299');
+	define('DB_PASS', 'UqYqkSyyDA');
+	define('DB_NAME', 'sql6416299');
